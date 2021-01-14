dade84a97a6f5aa0de62712be25c577ec3d9627b
erickes5trada <erick.e.senado@gmail.com>
2021-01-14 08:55:28 -0600

    importar materialize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 /* eslint-disable no-console */
 // Script del frontend
 // Importar estilos
-import './styles/styles.css';
+// import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
 
 console.log('Hello Modern JS Working!! Thanks BABEL....');
 // const show = (message = 'Saludo por defecto') => {
